ad1e95e4e47192f1afbb81aea0d5ae3664380d61
SORBO Michele 74428 @74428 <michele.sorbo@gmail.com>
2023-10-23 11:48:33 +0200

    OOP

--- Python/Esercizi/OOP/01_es.py
+++ Python/Esercizi/OOP/01_es.py
@@ -6,3 +6,26 @@ Ad esempio, se l’animale è un gatto dovrebbe stampare “Miao!”, se è un c
 Creare sempre il metodo per stampare la classe come stringa ( def __str__(self): )
 
 """
+
+class Animale:
+    def __init__(self, nome, specie):
+        self.nome = nome
+        self.specie = specie
+    
+    def __str__(self):
+        return f"Sono {self.nome} e sono un {self.specie}"
+
+    def emetti_suono(self):
+        if self.specie == "gatto":
+            return "Miao!"
+        elif self.specie == "cane":
+            return "Bau!"
+        else:
+            return "Non conosco la specie"
+
+cane1 = Animale("Fido", "cane")
+gatto1 = Animale("Fuffy", "gatto")
+
+print(f"{gatto1} il mio verso è {gatto1.emetti_suono()}")
+print(gatto1.emetti_suono())
+print(cane1.emetti_suono())
