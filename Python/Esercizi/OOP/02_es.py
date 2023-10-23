07d25fb15b6f56c45d1883f691d51c02660f0651
SORBO Michele 74428 @74428 <michele.sorbo@gmail.com>
2023-10-23 14:37:33 +0200

    OOP

--- Python/Esercizi/OOP/02_es.py
+++ Python/Esercizi/OOP/02_es.py
@@ -1,7 +1,40 @@
 """ 
 Esercizio 2
 Creare una classe Impiegato che abbia gli attributi “nome”, “cognome”, “matricola” e “stipendio”.
-Aggiungere un metodo “aumenta_stipendio” che aumenti lo stipendio dell’impiegato del 10% 
+Aggiungere un metodo “aumenta_stipendio” che aumenti lo stipendio dell’impiegato del 10% se non specificato altrimenti aumento lo stipendio
+della percentuale specificata
 e il metodo __str__ che stampi tutti i dettagli dell’impiegato, 
 ad esempio “Impiegato: Marco Rossi, matricola 12345, stipendio: 3000 Euro”. 
 """
+
+class Impiegato:
+    #1° cpsa da fare è il costruttore
+    def __init__(self, nome,cognome,matricola,stipendio):
+        self.nome = nome
+        self.cognome = cognome
+        self.matricola = matricola
+        self.stipendio = stipendio
+
+    #2° non oblbigatoria è definire il metodo __str__ così da evitare la stampa della posizione dell'oggetto in memoria
+    def __str__(self):
+        return f"Sono l'impiegato {self.nome} {self.cognome} con matricola {self.matricola}"
+
+    def getStimepndio(self):
+        return self.stipendio
+    
+    def aumenta_stipendio(self,aumento=10):
+        self.stipendio = (self.stipendio * (100+aumento))/100
+    
+
+i1 = Impiegato("Mario","Rossi","MT001",2300)
+i2 = Impiegato("Luca","Verdi","MT002",1700)
+
+print(i1)
+print(f"Stipendio impiegato 1 {i1.getStimepndio()}")
+i1.aumenta_stipendio(22)
+print(f"Stipendio impiegato 1 dopo aumento {i1.getStimepndio()}")
+
+print(i2)
+print(f"Stipendio impiegato 2 {i2.getStimepndio()}")
+i2.aumenta_stipendio()
+print(f"Stipendio impiegato 2 dopo aumento {i2.getStimepndio()}")
---
+++ Python/Esercizi/OOP/04_es.py
@@ -0,0 +1,17 @@
+"""
+Creare una classe ContoCorrente in grado di simulare un conto corrente reale, 
+quindi con versamenti, prelievi, stampa saldo e dammi la lista degli ultimi 5 movimenti.
+Costruttore: Saldo iniziale, Proprietario, N° Conto
+
+Fare tutto su un solo correntista
+
+Far partire una schermata con le opzioni es:
+Benvenuto nel tuo conto
+1) Versa
+2) Preleva
+3) Saldo Attuale
+4) Ultimi 5 movimenti (Da fare con l'array ATTENZIONE SOLO ULTIMI 5 MOVIMENTI)
+5) Info Conto (Restituisce Proprietario e N° Conto da fare con il metodo __str__)
+0) Esci
+
+"""
