9ccbc79d079259ed1a121074a8b46f70fedf4d70
SORBO Michele 74428 @74428 <michele.sorbo@gmail.com>
2023-11-14 12:23:25 +0100

    Django

--- Django/mysite/shop/urls.py
+++ Django/mysite/shop/urls.py
@@ -4,4 +4,6 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.lista_prodotti, name='lista_prodotti'),
+    path('<slug:categoria_slug>', views.lista_prodotti, name='product_list_by_category'), #è la vista dell apagina prodotti filtrata per categoria
+    path('<int:id>/<slug:slug>/', views.dettaglio_prodotto, name='dettaglio_prodotto'),#Serve per visualizzare un prodotto nel dettaglio
 ]
--- Django/mysite/shop/views.py
+++ Django/mysite/shop/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Categorie, Prodotti
 
 # Create your views here.
 
-def lista_prodotti(request):
+def lista_prodotti(request, categoria_slug=None):
+    categoria = None
     categorie = Categorie.objects.all()
-    prodotti = Prodotti.objects.all()
-    return render(request,'shop/lista_prodotti.html', {'categorie':categorie, 'prodotti':prodotti})
+    prodotti = Prodotti.objects.filter(pubblicato=True)
+    #Se viene passata uno slug di categoria facio i filtri
+    if categoria_slug:
+        categoria = get_object_or_404(Categorie, slug=categoria_slug)#Salvo in catogoria il nome dello slug
+        prodotti = Prodotti.objects.filter(categoria=categoria).filter(pubblicato=True)#Estraggo solo i prodotti della categoria passata
+    
+    return render(request,'shop/lista_prodotti.html', {'categorie':categorie, 'prodotti':prodotti, 'categoria':categoria})
+
+def dettaglio_prodotto(request, id, slug):
+    prodotto = get_object_or_404(Prodotti, id=id, slug=slug, pubblicato=True)
+    return render(request, 'shop/dettaglio_prodotto.html', {'prodotto':prodotto})
